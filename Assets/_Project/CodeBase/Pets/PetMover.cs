199f3faa11482468ac96f6f932fa8d70ec6ec60a
Нурлан Галиев <113087665+Nurlan1995G@users.noreply.github.com>
2024-08-31 23:56:09 +0400

    Skins player
    - Добавил PetMover, который следует за игрком
     - Добавил скины игрока в магазин, в rapidicon сделал спрайты для игрока. RewardModel скина игрока подгрузил

--- Assets/_Project/CodeBase/Pets/PetMover.cs
+++ Assets/_Project/CodeBase/Pets/PetMover.cs
@@ -2,28 +2,30 @@ using UnityEngine;
 
 public class PetMover : MonoBehaviour
 {
-    [SerializeField] private Transform _target;
-    [SerializeField] private float _approachTime;
+    [SerializeField] private Transform _targetPoint;
+    [SerializeField] private float _aproachTime;
     [SerializeField] private float _maxDistance;
 
     private float _timer;
 
     private void Start()
     {
-        transform.position = _target.position;
+        transform.position = _targetPoint.position;
     }
 
     private void Update()
     {
-        transform.forward = _target.forward;
+        transform.forward = _targetPoint.forward;
 
-        if (Vector3.Distance(transform.position, _target.position) >= _maxDistance)
+        if (Vector3.Distance(transform.position, _targetPoint.position) >= _maxDistance)
         {
-            transform.position = Vector3.Lerp(transform.position, _target.position, _timer / _approachTime);
+            transform.position = Vector3.Lerp(transform.position, _targetPoint.position, _timer / _aproachTime);
 
             _timer += Time.deltaTime;
         }
         else
+        {
             _timer = 0;
+        }
     }
 }
